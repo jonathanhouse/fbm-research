5ea3a8eb454e6fbb5b8bbb75cd0c66010448564d
jonathan <jhouse0615@gmail.com>
2024-04-29 08:33:41 -0500

    some organization & cleaning

---
+++ util-code/data_files1.py
@@ -0,0 +1,101 @@
+from os import walk
+import numpy as np
+
+loc = '/Users/jonhouse/Desktop/research/soft_fbm'
+
+class DataFile1: 
+
+    def __init__(self,path,grab_avx=True,grab_dis=True,grab_log=False,grab_out=False,grab_full=True):
+        self.path = path
+        self.avx = {}
+        self.dis = {}
+        self.log = {}
+        self.full = {}
+        self.gen_out = None # general purpose output 
+        self.tags = {}
+        self.params = {}
+
+        if (grab_out):
+            self.gen_out = self.make_data_dict('out')
+            return
+
+        if (grab_avx):
+            self.avx = self.make_data_dict('avx')
+        if (grab_dis):
+            self.dis = self.make_data_dict('dis')
+        if (grab_log):
+            self.log = self.make_data_dict('log')
+        if (grab_full):
+            if (bool(self.avx)):
+                self.full.update(self.avx)
+            if (bool(self.dis)):
+                self.full.update(self.dis)
+            if (bool(self.log)):
+                self.full.update(self.log)
+
+        
+
+    def file_parser(self, filename):
+        run_read = open(loc + '/' + self.path + filename,'r').readlines() # reads each line as a string 
+        num_lines = len(run_read)
+
+        if (filename == ""):
+            return run_read
+
+        type = filename[0:3] # some files might have longer identifiers so this is just arbitrary
+
+        offset = 0
+        for n in range(num_lines):
+            line = run_read[n].split()
+            for z in range(len(line)):
+                if (line[z][-1] == '=' and (z != len(line) - 1)): # if the param ends in an '=" and isn't the long string of them
+                    self.params[line[z][0:-1]] = line[z+1]
+                if (line[z] == '=================================='):
+                    offset = n 
+                    break
+        
+        header = run_read[offset + 1].split()
+        header_len = len(header)
+
+        data_spaces = num_lines - (offset + 2)
+        file_data = np.empty((header_len,data_spaces),float)
+
+        self.tags[type] = header
+        data_dict = {}
+
+        for k in range(data_spaces):
+            splits = run_read[k + offset + 2].split()
+            for i in range(len(splits)):
+                try:
+                    file_data[i,k] = splits[i]
+                except ValueError: 
+                    file_data[i,k] = 0
+            #file_data[:,k] = run_read[k + offset + 2].split()
+
+        for t in range(len(header)):
+            data_dict[header[t]] = file_data[t,:]
+
+        return data_dict
+
+
+
+    def make_data_dict(self,dtype):
+        if (dtype == 'out'):
+            return self.file_parser(str()) # don't need to find file because we just include it in path
+
+        full_path = loc + '/' + self.path
+        save_filename = ''
+        for dirpaths,dirnames,files in walk(full_path):
+            for f in files:
+                type = f[0:len(dtype)] 
+                if type == dtype:
+                    save_filename = f
+
+        if(save_filename == ''):
+            return None
+        else:
+            return self.file_parser('/' + save_filename)
+
+
+
+
---
+++ util-code/plot.py
@@ -0,0 +1,220 @@
+from parse_data import get_data,get_tempered_data, get_err_data, get_fast_data
+import matplotlib.pyplot as plt 
+import numpy as np
+from data_files import DataFile
+from numpy.polynomial.polynomial import Polynomial as Poly
+    
+
+def plot_binned(ax,fig,data, binsize,label,type='linear',linestyle=None):
+    L = data[0].length
+    for d in data: 
+
+       # if(d.length != L):
+        #    d.dis["P(x)"] = np.array( list(np.zeros(shape=int((L - d.length)/2))) + list(d.dis["P(x)"]) + list(np.zeros(shape=int((L - d.length)/2))) )
+        #    d.nbin = data[0].nbin
+         #   d.dis["ibin"] = data[0].dis["ibin"]
+
+        binned, bin_mid = ordered_binning(df=d,binsize=binsize)
+        return binned,bin_mid
+        if type == 'linear':
+            ax.set(xscale='linear',yscale='linear')
+            ax.set(xlabel='$x$',ylabel='$P(x)$')
+        if type == 'log':
+            ax.set(xscale='linear',yscale='log')
+            ax.set(xlabel='$x$',ylabel='$P(x)$')
+        if type == 'gaus':
+            ax.set(xscale='linear',yscale='log')
+            ax.set(xlabel='$x^2$',ylabel='$P(x)$')
+            bin_mid = bin_mid**2*np.sign(bin_mid)
+        ax.plot(bin_mid,binned,label=d.get_label(label),linestyle=linestyle)
+    ax.legend()
+
+    cols = len(data)
+    title = ["nconf","length","gamma","nt","weight","t"]
+    sup = "bin width=" + str(int(binsize)) + ", "
+    for t in title:
+        if t not in label: 
+            sup += str(data[cols-1].get_label(t)) + ", "
+    sup = sup[:-2]
+    fig.suptitle(t=sup)
+
+
+
+def ordered_binning(df, binsize):
+    px = df.dis["P(x)"]     
+    partitions = int(df.nbin*2/binsize)
+    binned_dis = np.zeros(shape=int(partitions))
+    midpoint_bins = np.zeros(shape=int(partitions))
+    for n in range(int(partitions)):
+        bin_start = binsize*n
+        binned_dis[n] = np.sum( px[bin_start:bin_start + binsize] )
+        midpoint_bins[n] = df.dis["ibin"][bin_start + int(0.5*binsize)]
+
+    return binned_dis, midpoint_bins
+
+def plot_times(ax,times,top):
+    for t in times:
+        ax.plot(t*np.ones(10),np.linspace(0,top,10))
+
+def msd_fit(ax, data, interval):
+
+    bound_arr = data[0].avx['t']
+    low_bound = np.nonzero(np.fabs( bound_arr - interval[0]) < 1e-10)[0][0]
+    high_bound = np.nonzero(np.fabs( bound_arr - interval[1]) < 1e-10)[0][0]
+    for p in data: 
+
+
+
+        x_test = np.linspace(np.log10(interval[0]),np.log10(interval[1]),100)
+
+        series = Poly.fit(np.log10(p.avx['t'][low_bound:high_bound]), np.log10(p.avx['<r^2>'][low_bound:high_bound]), deg=1, window=None)
+        series = series.convert()
+
+        p.series = series
+
+        ax.plot(10**x_test, 10**series(x_test),marker='x',markersize=3,label=str(p.series))
+        ax.legend()
+
+
+def log_fit(ax,data_x,data_y,interval,plot_fit = True):
+    bound_arr = data_x
+    low_bound = np.nonzero(np.fabs( bound_arr - interval[0]) < 1e-7)[0][0]
+    high_bound = np.nonzero(np.fabs( bound_arr - interval[1]) < 1e-7)[0][0]
+
+    x_test = np.linspace(np.log10(interval[0]),np.log10(interval[1]),100)
+    series = Poly.fit(np.log10(data_x[low_bound:high_bound]), np.log10(data_y[low_bound:high_bound]), deg=1, window=None)
+    series = series.convert()
+
+    if (True):
+        ax.plot(10**x_test, 10**series(x_test),marker='*',label=series)
+        #ax.plot(x_test, series(x_test),marker='x',markersize=3)
+
+    ax.legend()
+
+    return series 
+
+def gen_fit(ax,data_x,data_y,interval):
+    bound_arr = data_x
+
+    dif_low = abs(bound_arr[0] - interval[0])
+    dif_high = abs(bound_arr[0] - interval[1])
+    low_val = bound_arr[0]
+    high_val = bound_arr[0]
+
+    for x in data_x:
+        if (abs(x - interval[0]) < dif_low):
+            dif_low = abs(x - interval[0])
+            low_val = x
+        if (abs(x - interval[1]) < dif_high):
+            dif_high = abs(x - interval[1])
+            high_val = x
+
+    print(low_val,high_val)
+    low_bound = np.nonzero(np.fabs( bound_arr - low_val) < 1e-7)[0][0]
+    high_bound = np.nonzero(np.fabs( bound_arr - high_val) < 1e-7)[0][0]
+
+    x_test = np.linspace(interval[0],interval[1],100)
+    series = Poly.fit(data_x[low_bound:high_bound], data_y[low_bound:high_bound], deg=1, window=None)
+    series = series.convert()
+    series = series.convert()
+
+    ax.plot(x_test, series(x_test),markersize=3,label=series,linestyle='dashed')
+    ax.legend()
+
+    return series 
+
+
+
+def plot(ax, fig, data, type, label,marker=None,xlim=None,markersize=None,linestyle=None,ylim=None):
+
+    if(isinstance(ax,plt.Axes)): 
+       
+        cols = len(data)
+        if(type == 'msd'): 
+            ax.set(xscale = 'log', yscale = 'log')
+            for p in data: 
+
+                s = ""
+                for l in label:
+                    s += p.get_label(l) + ", "
+                s = s[:-2]
+
+                ax.plot(p.avx['t'], p.avx['<r^2>'],label=s,marker=marker,linestyle=linestyle)
+                ax.set(xlabel='$t$',ylabel='$\\langle x^2 \\rangle$')
+
+        if (type == 'dis'):
+            ax.set_xlim(xlim)
+            ax.set_ylim(ylim)
+            for p in data: 
+                
+                s = ""
+                for l in label:
+                    s += p.get_label(l) + ", "
+                s = s[:-2]
+
+                ax.plot(p.dis['x'], p.dis['P(x)'], label=s,marker=marker,markersize=markersize,linestyle=linestyle)
+                ax.set( xlabel='$x$',ylabel='$P(x)$')
+
+
+        if (type == 'gaus'):
+            ax.set_xlim(xlim)
+            ax.set_ylim(ylim)
+            ax.set(xscale = 'linear', yscale = 'log')
+            for p in data: 
+                
+                s = ""
+                for l in label:
+                    s += p.get_label(l) + ", "
+                s = s[:-2]
+
+                ax.plot(p.dis['x']**2*np.sign(p.dis['x']), p.dis['P(x)'], label=s,marker=marker,markersize=markersize,linestyle=linestyle)
+                ax.set( xlabel='$x^{2}$',ylabel='$P(x)$')
+
+
+        ax.legend()
+
+        title = ["nconf","length","gamma","nt","weight","t"]
+        sup = ""
+        for t in title:
+            if t not in label: 
+                sup += str(data[cols-1].get_label(t)) + ", "
+        sup = sup[:-2]
+        fig.suptitle(t=sup)
+
+
+    else:  
+
+        cols = len(ax)
+        for i in range(cols):
+
+            if(type[i] == 'msd'): 
+                ax[i].set(xscale = 'log', yscale = 'log')
+                for p in data[i]: 
+                    ax[i].plot(p.avx['t'], p.avx['<r^2>'],label= p.get_label(label[i]))
+                    ax[i].set(xlabel='$t$',ylabel='$\\langle x^2 \\rangle$')
+
+            if (type[i] == 'dis'):
+                ax[i].set_ylim(ylim)
+                ax[i].set_xlim(xlim)
+                for p in data[i]: 
+                    ax[i].plot(p.dis['x'], p.dis['P(x)'], label=p.get_label(label[i]))
+                    ax[i].set( xlabel='$x$',ylabel='$P(x)$')
+
+            if (type[i] == 'gaus'):
+                ax[i].set_xlim(xlim)
+                ax[i].set_ylim(ylim)
+                ax[i].set(xscale = 'linear', yscale = 'log')
+                for p in data[i]: 
+
+                    ax[i].plot(p.dis['x']**2*np.sign(p.dis['x']), p.dis['P(x)'], label=p.get_label(label[i]))
+                    ax[i].set( xlabel='$x^{2}$',ylabel='$P(x)$')
+
+            ax[i].legend()
+
+        title = ["nconf","length","gamma","nt","weight","t"]
+        sup = ""
+        for t in title:
+            if t not in label: 
+                sup += str(data[cols-1][0].get_label(t)) + ", "
+        sup = sup[:-2]
+        fig.suptitle(t=sup)
